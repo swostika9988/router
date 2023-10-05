fea00cbb4b9e76e6adb172ef195dce6d58b7449f
swostika9988 <sostikamahato40@gmail.com>
2023-10-05 13:31:27 +0545

    toggleValue

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Service() {
+  const [check,setCheck]=useState(true);
+  const toggleValue=()=>{
+  setCheck(prev=>!prev);
+  }
+
   return (
-    <div>Service</div>
+    <>
+    <div>
+      {check?"true":"false"}
+    </div>
+    <button onClick={toggleValue}>
+      Toggle
+    </button>
+    </>
   )
 }
 
